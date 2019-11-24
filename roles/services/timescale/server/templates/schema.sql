b21a8b336e08aefa76d496cf1410ce065b556d34
ssube <seansube@gmail.com>
2019-11-24 14:36:12 -0600

    feat: add node problem detector, move prometheus adapter to monitoring

--- roles/services/timescale/server/templates/schema.sql
+++ roles/services/timescale/server/templates/schema.sql
@@ -1,14 +1,16 @@
 CREATE TABLE IF NOT EXISTS metrics (
-  time timestamp,
-  name text,
-  value float,
+  time timestamp NOT NULL,
+  name text NOT NULL,
+  value float NOT NULL,
   labels jsonb
 );
 
 SELECT create_hypertable(
   'metrics',
   'time',
-  chunk_time_interval => '1h',
+  chunk_time_interval => interval '1h',
   create_default_indexes => TRUE,
-  if_not_exists => TRUE
+  if_not_exists => TRUE,
+  partitioning_column => 'name',
+  number_partitions => 4
 );
