f3de27d30ca38a8406f0228d6c7e4e60d53f9f0c
ssube <seansube@gmail.com>
2019-12-13 06:27:42 -0600

    fix(services/timescale): prune after 30 days

--- scripts/timescale-prune.sh
+++ scripts/timescale-prune.sh
@@ -9,15 +9,16 @@ source "$(dirname ${BASH_SOURCE[0]})/common.sh"
 ###
 
 prune_table="${1}" && shift
+prune_database="${1}" && shift
 prune_domain="${1}" && shift
-prune_hours="${1:-12 hours}"
-prune_query="SELECT drop_chunks(interval '${prune_hours}', '${prune_table}');"
+prune_hours="${1:-30 days}"
+prune_query="SELECT drop_chunks(interval '${prune_hours}', '${prune_table}', cascade_to_materializations=>TRUE);"
 
 echo_color start "Pruning chunks older than ${prune_hours}"
 
 echo_color info "Finding pod for ${prune_domain}/timescale-role=server"
 timescale_pod="$(find_pod server monitor ${prune_domain}/timescale-role)"
 echo_color info "Running query in pod: ${timescale_pod}"
-kubectl exec -n monitor ${timescale_pod} -it -- psql -U postgres -d prometheus -c "${prune_query}"
+kubectl exec -n monitor ${timescale_pod} -it -- psql -U postgres -d ${prune_database} -c "${prune_query}"
 
 echo_color success "Pruned chunks"
